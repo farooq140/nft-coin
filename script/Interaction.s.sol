5132607411dfc2a9ce1fee7192c6280db511f590
Farooq <farooqahmed21001@gmail.com>
2024-09-14 18:43:16 +0500

    modules

---
+++ script/Interaction.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    string constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentContract = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        mintNft(mostRecentContract);
+    }
+
+    function mintNft(address basicNftAddress) public {
+        vm.startBroadcast();
+        BasicNft(basicNftAddress).mintNft(PUG_URI);
+        vm.stopBroadcast();
+    }
+}
---
+++ test/DeployMoodNftTest.t.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public deployer;
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+    }
+
+    function testConvertSvgToUri() public view {
+        string memory expectedUri =
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIj4NCiAgPHRleHQgeD0iMjAwIiB5PSIyNTAiIGZpbGw9ImJsYWNrIj4NCiAgICBIaSEgWW91IGRlY29kZWQgdGhpcyF7IiAifQ0KICA8L3RleHQ+DQo8L3N2Zz4NCg0KDQoNCg0KDQoNCg0KDQoNCg0KDQoNCg0KDQoNCg0KDQoNCg0K";
+        string memory svg =
+            '<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"500\" height=\"500\"><text x=\"200\" y=\"250\" fill=\"black\">Hi! You decoded this!</text></svg>';
+
+        string memory actualUri = deployer.svgToImageUri(svg);
+        console.log("actual",actualUri,"expectUri", expectedUri);
+        assert(keccak256(abi.encodePacked(expectedUri)) == 
+            keccak256(abi.encodePacked(actualUri))
+            );
+    }
+}
